0bdc0367f093c0644aaafed6f13567bb90657879
stevenmz <smaganazook@live.com>
2014-08-15 16:10:50 -0700

    Added a unit test of the example provided in the readme file

--- src/test/java/stevenmz/pso/test/PSOTests.java
+++ src/test/java/stevenmz/pso/test/PSOTests.java
@@ -84,4 +84,57 @@ public class PSOTests {
         Assert.assertEquals(0, bestSolution[0].intValue());
         Assert.assertEquals(0, bestSolution[1].intValue());
     }
+
+    @Test
+    public void AnotherExamplePolynomialMinimizationTest() {
+    	// set the hyper-parameters used by the particle swarm optimization algorithm.
+    	// Experiment with different values to see the rate of execution and accuracy
+        final int NUMBER_OF_PARTICLES = 10; 
+        final int NUMBER_OF_ITERATIONS = 100;
+        
+        // Define the function we want the algorithm to optimize
+        final ParticleSwarmOptimizer.IParticleFunctionEvaluator functionToMinimize = new ParticleSwarmOptimizer.IParticleFunctionEvaluator() {
+            /**
+             * This method performs the evaluation of the function we want to optimize
+             * Minimize: f(a,b,c) = 1234 + a^2 + b^2 + c^2
+             */
+        	@Override
+            public double Evaluate(Number... potentialSolution) {
+                return 1234
+                        + Math.pow(potentialSolution[0].intValue(), 2)
+                        + Math.pow(potentialSolution[1].intValue(), 2)
+                        + Math.pow(potentialSolution[2].intValue(), 2);
+            }
+
+            /**
+             * This method is used to initialize particles to random locations 
+             * throughout the problem space when the swarm begins the optimization routine. 
+             */
+            @Override
+            public Number[] getRandomSolution() {
+                Random r = new Random();
+                Number[] solution = new Number[3];
+                solution[0] = r.nextInt();
+                solution[1] = r.nextInt();
+                solution[2] = r.nextInt();
+                return solution;
+            }
+        };
+
+        ParticleSwarmOptimizer optimizer = new ParticleSwarmOptimizer(
+                functionToMinimize,
+                NUMBER_OF_PARTICLES,
+                NUMBER_OF_ITERATIONS,
+                ParticleSwarmOptimizer.OptimizationType.MINIMIZE);
+
+        // Perform the optimization
+        ParticleSwarmOptimizer.Particle optimalPartical = optimizer.optimize();
+
+        // Retrieve the solution computed by the algorithm
+        final Number[] bestSolution = optimalPartical.getLocalBestSolution();
+
+        Assert.assertEquals(0, bestSolution[0].intValue());
+        Assert.assertEquals(0, bestSolution[1].intValue());
+        Assert.assertEquals(0, bestSolution[2].intValue());
+    }
 }
